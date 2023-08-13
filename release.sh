90330f77c404de10ac1bae1e9d6dd86ba68d67d4
Jim Zucker <jim@zucker.us>
2023-08-13 10:32:44 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -30,7 +30,7 @@ case "$TEST_OPTION" in
 
 
 	"RELEASE_CHECK" | "RELEASE_NO_CHECK")
-		echo "Step 1 Check if releaes exit if it does fail"
+		echo "Step 1 Check if releases exit if it does fail."
 		EXISTING_TAG=`git tag --list | egrep "$RELEASE_VERSION"`
 		if [ "$EXISTING_TAG" != "" ]
 		then
@@ -40,16 +40,16 @@ case "$TEST_OPTION" in
 
 		if [ "$TEST_OPTION" = "RELEASE_CHECK" ]
 		then
-			#for demoo purposes
+			#for demo purposes
 			cp module1/pom.xml module1/pom.xml.bak
 			sed 's/3.8.1/5.11.0-SNAPSHOT/g' module1/pom.xml.bak >module1/pom.xml
 
-			echo "Step 2 check if we have any snapshots, fail build if we do"
+			echo "Step 2 check if we have any snapshots, fail build if we do."
 			SNAPSHOT_DEPENDENCIES=`find . -name pom.xml -exec egrep -H SNAPSHOT {} \;|egrep -v '<revision>.*\..*\..*-SNAPSHOT</revision>'`
 			#echo "SNAPSHOT_DEPENDENCIES=$SNAPSHOT_DEPENDENCIES"
 			if [ "$SNAPSHOT_DEPENDENCIES" != "" ]
 			then
-				echo "ERROR: cannot release with SNAPSHOT dependencies"
+				echo "ERROR: cannot release with SNAPSHOT dependencies."
 				echo "------------------------------------------------"
 				echo $SNAPSHOT_DEPENDENCIES
 				echo "------------------------------------------------"
@@ -64,14 +64,14 @@ case "$TEST_OPTION" in
 		fi
 
 		set -e
-		echo "Step 3 build branch - fail if build fails for compile or tests we save pushing artifacts"
+		echo "Step 3 build branch - fail if the build fails for compile or tests we save pushing artifacts."
 		mvn -Drevision="$RELEASE_VERSION" clean install 
 
-		echo "Step 4 if build passes push to nexus"
+		echo "Step 4 if the build passes push to nexus."
 		mvn -Drevision="$RELEASE_VERSION" -DskipTests validate
 		#mvn -Drevision=$RELEASE_VERSION -DskipTests deploy
 
-		echo "Step 4a Validate maven artifacts"
+		echo "Step 4a Validate maven artifacts."
 		mvn -Drevision="$RELEASE_VERSION" -DskipTests -P testRelease install
 
 		echo "Step 5 - Tag the release and push to repo, tag: $RELEASE_VERSION"
@@ -79,8 +79,8 @@ case "$TEST_OPTION" in
 		git push origin $RELEASE_VERSION
 	;;
 
-	*) echo "BRANCH release"
-		echo "Step 3 build branch - fail if build fails for compile or tests we save pushing artifacts"
+	*) echo "BRANCH release."
+		echo "Step 3 build branch - fail if the build fails for compile or tests we save pushing artifacts."
 		#mvn -Drevision=$BRANCH_VERSION clean install deploy
 		mvn -Drevision=$BRANCH_VERSION clean install 
 	;;
