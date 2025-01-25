0fb8ea6fffcf3c4a7c6b03cdba9f8bfddc784159
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2025-01-25 10:02:06 +0530

    My new commit

---
+++ module1/src/main/java/io/jitpack/AppOneDotOne.java
@@ -0,0 +1,15 @@
+package io.jitpack;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppOneDotOne
+{
+    public static final String GREETING = "Hello World!";
+    
+    public static void main( String[] args )
+    {
+        System.out.println(GREETING);
+    }
+}
